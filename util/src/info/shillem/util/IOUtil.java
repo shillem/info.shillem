1a13cf3788d3b630a70c433c6395d304c96ac05a
Shillem Volpato <volpato.sh@gmail.com>
2019-06-13 12:05:17 +0200

    Added util method to copy stream into a byte array

--- util/src/info/shillem/util/IOUtil.java
+++ util/src/info/shillem/util/IOUtil.java
@@ -1,11 +1,14 @@
 package info.shillem.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
 public enum IOUtil {
-	;
-	
+    ;
+
     public static void close(Closeable c) {
         if (c != null) {
             try {
@@ -23,5 +26,18 @@ public enum IOUtil {
             Arrays.stream(others).forEach(IOUtil::close);
         }
     }
-    
+
+    public static byte[] readAllBytes(InputStream is) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        byte[] data = new byte[4096];
+        int len;
+
+        while ((len = is.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, len);
+        }
+
+        return buffer.toByteArray();
+    }
+
 }
