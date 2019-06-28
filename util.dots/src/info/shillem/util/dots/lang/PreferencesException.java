5a062ee39fd8be0baf55ae18209faa2079ee402e
Shillem Volpato <volpato.sh@gmail.com>
2019-06-28 16:59:41 +0200

    [DOTS] Silenced PreferencesException stacktrace

--- util.dots/src/info/shillem/util/dots/lang/PreferencesException.java
+++ util.dots/src/info/shillem/util/dots/lang/PreferencesException.java
@@ -4,16 +4,13 @@ public class PreferencesException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public PreferencesException(Exception e) {
-        super(e);
-    }
-
     public PreferencesException(String message) {
         super(message);
     }
 
-    public PreferencesException(String message, Exception e) {
-        super(message, e);
+    @Override
+    public Throwable fillInStackTrace() {
+        return this;
     }
-
+    
 }
