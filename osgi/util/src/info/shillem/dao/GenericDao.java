a9639af98b9dd08ac3876754af758d587a5d21ee
Shillem Volpato <volpato.sh@gmail.com>
2020-11-24 16:31:10 +0100

    Added GenericDao interface default method responses

--- osgi/util/src/info/shillem/dao/GenericDao.java
+++ osgi/util/src/info/shillem/dao/GenericDao.java
@@ -8,18 +8,32 @@ import info.shillem.dto.BaseField;
 
 public interface GenericDao<T extends BaseDto<E>, E extends Enum<E> & BaseField> {
 
-    List<T> collect(Query<E> query) throws DaoException;
+    default List<T> collect(Query<E> query) throws DaoException {
+        throw new UnsupportedOperationException();
+    }
 
-    void create(T dto) throws DaoException;
+    default void create(T dto) throws DaoException {
+        throw new UnsupportedOperationException();
+    }
 
-    void delete(Query<E> query) throws DaoException;
+    default void delete(Query<E> query) throws DaoException {
+        throw new UnsupportedOperationException();
+    }
 
-    T read(Query<E> query) throws DaoException;
+    default T read(Query<E> query) throws DaoException {
+        throw new UnsupportedOperationException();
+    }
 
-    void update(List<T> dtos) throws DaoException;
+    default void update(List<T> dtos) throws DaoException {
+        throw new UnsupportedOperationException();
+    }
 
-    void update(T dto) throws DaoException;
+    default void update(T dto) throws DaoException {
+        throw new UnsupportedOperationException();
+    }
 
-    void upsert(List<T> dtos) throws DaoException;
+    default void upsert(List<T> dtos) throws DaoException {
+        throw new UnsupportedOperationException();
+    }
 
 }
