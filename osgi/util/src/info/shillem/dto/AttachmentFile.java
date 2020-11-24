6f23d2d2fa56bd0550f4364fac3591935dfae261
Shillem Volpato <volpato.sh@gmail.com>
2020-11-24 16:30:50 +0100

    Added a few props to AttachmentFile in order to store optional stream and size information

--- osgi/util/src/info/shillem/dto/AttachmentFile.java
+++ osgi/util/src/info/shillem/dto/AttachmentFile.java
@@ -1,6 +1,7 @@
 package info.shillem.dto;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -9,8 +10,11 @@ public class AttachmentFile implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private final String name;
+
     private File file;
+    private InputStream inputStream;
     private boolean remove;
+    private Long size;
 
     public AttachmentFile(String name) {
         this(name, null);
@@ -25,8 +29,16 @@ public class AttachmentFile implements Serializable {
         return file;
     }
 
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public Long getSize() {
+        return size;
     }
 
     public boolean isRemove() {
@@ -37,8 +49,16 @@ public class AttachmentFile implements Serializable {
         file = value;
     }
 
+    public void setInputStream(InputStream value) {
+        inputStream = value;
+    }
+
     public void setRemove(Boolean value) {
         remove = value;
+    }
+
+    public void setSize(Long value) {
+        size = value;
     }
 
     public void toggleRemove() {
