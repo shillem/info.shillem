18c6e1eceb3ca32b3b5b13ef0f8902fe52c4427e
Shillem Volpato <volpato.sh@gmail.com>
2021-02-05 13:43:44 +0100

    Improved DataSourceService to implement DBCP properties when not managed

--- osgi/sql/src/info/shillem/sql/DataSourceService.java
+++ osgi/sql/src/info/shillem/sql/DataSourceService.java
@@ -1,12 +1,13 @@
 package info.shillem.sql;
 
 import java.sql.Driver;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 import javax.sql.DataSource;
 
@@ -15,10 +16,12 @@ import org.apache.commons.dbcp2.DriverConnectionFactory;
 import org.apache.commons.dbcp2.PoolableConnection;
 import org.apache.commons.dbcp2.PoolableConnectionFactory;
 import org.apache.commons.dbcp2.PoolingDataSource;
-import org.apache.commons.pool2.ObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 
 public class DataSourceService {
+
+    private static final List<String> INTERNAL_MANDATORY_KEYS =
+            Arrays.asList("name", "driverClassName", "url");
 
     private final Function<String, Driver> loader;
     private final Map<String, DataSource> sources;
@@ -28,44 +31,95 @@ public class DataSourceService {
         this.sources = new HashMap<>();
     }
 
-    public DataSource getDataSource(Properties properties) {
-        if (!properties.containsKey("name")) {
-            throw new IllegalArgumentException("'name' property is mandatory");
+    private DriverConnectionFactory createDriverConnectionFactory(Properties properties) {
+        String driverClassName = properties.getProperty("driverClassName");
+        Driver connectionDriver = loader.apply(driverClassName);
+
+        if (connectionDriver == null) {
+            throw new IllegalArgumentException(driverClassName + " driver is unavailable");
         }
 
-        if (!properties.containsKey("driverClassName")) {
-            throw new IllegalArgumentException("'driverClassName' property is mandatory");
-        }
+        Properties connectionProperties = new Properties();
 
-        if (!properties.containsKey("url")) {
-            throw new IllegalArgumentException("'url' property is mandatory");
-        }
+        for (Map.Entry<?, ?> entry : properties.entrySet()) {
+            String prop = (String) entry.getKey();
 
-        return sources.computeIfAbsent(properties.getProperty("name"), (k) -> {
-            String driverClassName = properties.getProperty("driverClassName");
-            Driver connectionDriver = loader.apply(driverClassName);
-
-            if (connectionDriver == null) {
-                throw new IllegalArgumentException(driverClassName + " driver is unavailable");
+            if (!prop.startsWith("connection.")) {
+                continue;
             }
 
-            Properties connectionProperties = new Properties();
+            connectionProperties.setProperty(
+                    prop.replace("connection.", ""), (String) entry.getValue());
+        }
 
-            Stream.of("username", "password", "connectionProperties").forEach((p) -> {
-                if (properties.containsKey(p)) {
-                    connectionProperties.setProperty(p, properties.getProperty(p));
-                }
-            });
+        return new DriverConnectionFactory(
+                connectionDriver,
+                properties.getProperty("url"),
+                connectionProperties);
+    }
 
-            ConnectionFactory connectionFactory = new DriverConnectionFactory(
-                    connectionDriver,
-                    properties.getProperty("url"),
-                    connectionProperties);
-            PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(
-                    connectionFactory,
-                    null);
-            ObjectPool<PoolableConnection> pool = new GenericObjectPool<>(
-                    poolableConnectionFactory);
+    private GenericObjectPool<PoolableConnection> createGenericObjectPool(
+            PoolableConnectionFactory connectionFactory,
+            Properties properties) {
+        GenericObjectPool<PoolableConnection> pool = new GenericObjectPool<>(connectionFactory);
+
+        for (Map.Entry<?, ?> entry : properties.entrySet()) {
+            String prop = (String) entry.getKey();
+
+            switch (prop) {
+            case "pool.maxTotal":
+                pool.setMaxTotal(Integer.valueOf((String) entry.getValue()));
+                break;
+            case "pool.maxWaitMillis":
+                pool.setMaxWaitMillis(Long.valueOf((String) entry.getValue()));
+                break;
+            case "pool.testOnBorrow":
+                pool.setTestOnBorrow(Boolean.valueOf((String) entry.getValue()));
+                break;
+            }
+        }
+
+        return pool;
+    }
+
+    private PoolableConnectionFactory createPoolableConnectionFactory(
+            ConnectionFactory connectionFactory,
+            Properties properties) {
+        PoolableConnectionFactory poolable = new PoolableConnectionFactory(connectionFactory, null);
+
+        for (Map.Entry<?, ?> entry : properties.entrySet()) {
+            String prop = (String) entry.getKey();
+
+            switch (prop) {
+            case "pool.defaultQueryTimeout":
+                poolable.setDefaultQueryTimeout(Integer.valueOf((String) entry.getValue()));
+                break;
+            case "pool.validationQuery":
+                poolable.setValidationQuery((String) entry.getValue());
+                break;
+            case "pool.validationQueryTimeout":
+                poolable.setValidationQueryTimeout(Integer.valueOf((String) entry.getValue()));
+                break;
+            }
+        }
+
+        return poolable;
+    }
+
+    public DataSource getDataSource(Properties properties) {
+        INTERNAL_MANDATORY_KEYS.forEach((k) -> {
+            if (!properties.containsKey(k)) {
+                throw new IllegalArgumentException(String.format("'%s' property is mandatory", k));
+            }
+        });
+
+        return sources.computeIfAbsent(properties.getProperty("name"), (k) -> {
+            DriverConnectionFactory connection =
+                    createDriverConnectionFactory(properties);
+            PoolableConnectionFactory poolable =
+                    createPoolableConnectionFactory(connection, properties);
+            GenericObjectPool<PoolableConnection> pool =
+                    createGenericObjectPool(poolable, properties);
 
             return new PoolingDataSource<PoolableConnection>(pool);
         });
