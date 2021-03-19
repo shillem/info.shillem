b2bb494c2107d22a1d94a8d9a627b630267de231
Shillem Volpato <volpato.sh@gmail.com>
2021-03-19 15:30:01 +0100

    Refactored/added helper classes of VwPath to make things easier in the DAO implementation layer

--- osgi/domino/src/info/shillem/domino/util/VwPath.java
+++ osgi/domino/src/info/shillem/domino/util/VwPath.java
@@ -1,7 +1,66 @@
 package info.shillem.domino.util;
 
-public interface VwPath {
+public class VwPath {
+    
+    public static class Builder {
 
-    String getName();
+        private final String alias;
+        private final String name;
+        
+        private VwRowAccessPreference rowAccessPreference;
+        private VwType type;
+        
+        public Builder(String alias, String name) {
+            this.alias = alias;
+            this.name = name;
+            this.rowAccessPreference = VwRowAccessPreference.DOCUMENT;
+            this.type = VwType.BACK_END;
+        }
+        
+        public VwPath build() {
+            return new VwPath(this);
+        }
+        
+        public Builder setRowAccessPreference(VwRowAccessPreference value) {
+            rowAccessPreference = value;
+            
+            return this;
+        }
+        
+        public Builder setType(VwType value) {
+            type = value;
+            
+            return this;
+        }
+        
+    }
+
+    private final String name;
+    private final String alias;
+    private final VwRowAccessPreference rowAccessPreference;
+    private final VwType type;
+    
+    public VwPath(Builder builder) {
+        name = builder.name;
+        alias = builder.alias;
+        rowAccessPreference = builder.rowAccessPreference;
+        type = builder.type;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public VwRowAccessPreference getRowAccessPreference() {
+        return rowAccessPreference;
+    }
+    
+    public VwType getType() {
+        return type;
+    }
 
 }
---
+++ osgi/domino/src/info/shillem/domino/util/VwType.java
@@ -0,0 +1,6 @@
+package info.shillem.domino.util;
+
+public enum VwType {
+    BACK_END,
+    FRONT_END;
+}
