17364eed96a1c3091cfefce680fb397f4dbddedd
Shillem Volpato <volpato.sh@gmail.com>
2021-11-23 12:00:36 +0100

    Added NumberUtil utility method to know what decimal separator is used based on a given locale

--- osgi/util/src/info/shillem/util/NumberUtil.java
+++ osgi/util/src/info/shillem/util/NumberUtil.java
@@ -1,6 +1,9 @@
 package info.shillem.util;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class NumberUtil {
 
@@ -10,10 +13,17 @@ public class NumberUtil {
         throw new UnsupportedOperationException();
     }
 
+    public static char getDecimalSeparator(Locale locale) {
+        DecimalFormat format = (DecimalFormat) DecimalFormat.getInstance(locale);
+        DecimalFormatSymbols symbols = format.getDecimalFormatSymbols();
+        
+        return symbols.getDecimalSeparator();
+    }
+
     public static BigDecimal percentageOf(BigDecimal value, BigDecimal percentage) {
         return value.multiply(percentage.divide(BIG_DECIMAL_HUNDRED));
     }
-
+    
     public static BigDecimal subtractPercentageOf(BigDecimal value, BigDecimal percentage) {
         return value.subtract(percentageOf(value, percentage));
     }
